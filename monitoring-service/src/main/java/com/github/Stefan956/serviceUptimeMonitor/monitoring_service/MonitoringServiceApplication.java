6db6578d00b118a1b0680b784a82afb6a61aa706
Stefan956 <stefan.milenov.todorov@gmail.com>
2026-01-06 17:19:16 +0200

    Creation of the initial project structure

---
+++ monitoring-service/src/main/java/com/github/Stefan956/serviceUptimeMonitor/monitoring_service/MonitoringServiceApplication.java
@@ -0,0 +1,13 @@
+package com.github.Stefan956.serviceUptimeMonitor.monitoring_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitoringServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitoringServiceApplication.class, args);
+	}
+
+}
